0ed597736375e6d337756e9488736ba81da88016
roberrera <roberterrera@gmail.com>
2016-04-15 19:47:34 -0400

    Say 'Hello, world!' the long way.

--- app/src/main/java/com/roberterrera/rxandroidpractice/MainActivity.java
+++ app/src/main/java/com/roberterrera/rxandroidpractice/MainActivity.java
@@ -2,6 +2,15 @@ package com.roberterrera.rxandroidpractice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Observer;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.Subscription;
+import rx.android.schedulers.AndroidSchedulers;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // The long way to "Hello, world!"
+        Observable<String> myObservable = Observable.create(
+                new Observable.OnSubscribe<String>() {
+                    @Override
+                    public void call(Subscriber<? super String> sub) {
+                        sub.onNext("Hello, world!");
+                        sub.onCompleted();
+                    }
+                }
+        );
+
+        Subscriber<String> mySubscriber = new Subscriber<String>() {
+            @Override
+            public void onNext(String s) {
+                System.out.println(s);
+            }
+
+            @Override
+            public void onCompleted() { }
+
+            @Override
+            public void onError(Throwable e) { }
+        };
+
+        // Outputs "Hello, world!"
+        myObservable.subscribe(mySubscriber);
+
+
     }
 }
